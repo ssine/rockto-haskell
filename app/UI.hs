c185f29f531e7ab384cfd393a1ee48662b8d61af
shijiesun-edu <shs001@ucsd.edu>
2021-11-28 14:54:59 +0800

    show map tiles in 2d

--- app/UI.hs
+++ app/UI.hs
@@ -3,10 +3,12 @@ module UI where
 import Rockto.Types
 import qualified Rockto as R
 
-import Control.Monad (void, forever)
-import Graphics.Vty as Vty
+import qualified Data.List as L
 
-import Brick (Widget, simpleMain, (<+>), str, withBorderStyle)
+import Control.Monad (void, forever)
+import qualified Graphics.Vty as V
+
+import Brick (Widget, (<+>), str, withBorderStyle)
 import Brick.Widgets.Center (center)
 import Brick.Widgets.Border (borderWithLabel, vBorder)
 import Brick.Widgets.Border.Style (unicode)
@@ -16,7 +18,10 @@ drawUI st = [ui]
   where
     ui = withBorderStyle unicode $
          borderWithLabel (str R.appName) $
-         (center (str . show . getMap $ _map st)
+         (center (str
+                  . L.intercalate "\n"
+                  . map show
+                  . getMap $ _map st)
            <+> vBorder
            <+> center (str $ "Round: " ++ (show . _round $ st)
                         ++ "\nScore: " ++ (show . _score $ st)
