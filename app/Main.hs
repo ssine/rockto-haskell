32c62349f4524f58d58e403e48ff0d057f3cbbee
shijiesun-edu <shs001@ucsd.edu>
2021-11-28 23:26:38 +0800

    add delayed callback when stable=False

--- app/Main.hs
+++ app/Main.hs
@@ -2,52 +2,67 @@
 
 module Main where
 
+import Rockto.Tick (tick)
+import Rockto.Types (Direction (..), GSt (_stable))
+import Rockto.Utils (mkInitS)
 import UI (drawUI)
 
-import Rockto.Tick (tick)
-import Rockto.Types
-import Rockto.Utils (mkInitS)
-
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever, void)
+import Control.Monad.IO.Class (liftIO)
 
 import qualified Graphics.Vty as V
 import qualified System.Random as R (newStdGen)
 
-import Brick
+import Brick (App (..), AttrMap, BrickEvent (VtyEvent), EventM, Next, attrMap, continue,
+              defaultMain, fg, halt, neverShowCursor, on)
 import Brick.AttrMap (AttrMap, attrMap)
 import Brick.Main (App (..), defaultMain, neverShowCursor)
 import Brick.Util (fg, on)
 
-import Control.Monad (forever, void)
-
 
 theMap :: AttrMap
-theMap = attrMap V.defAttr
-    [ ("keyword1",      fg V.magenta)
-    , ("keyword2",      V.white `on` V.blue)
-    ]
+theMap =
+  attrMap
+    V.defAttr
+    [("keyword1", fg V.magenta), ("keyword2", V.white `on` V.blue)]
+
+-- Run delayed callback when `stable` of the returned state is `False`
+doTick :: Rockto.Types.Direction -> GSt -> EventM () (Next GSt)
+doTick direction st =
+  let nst = tick direction st
+   in if _stable nst
+        then continue nst
+        else do
+          liftIO (threadDelay 1000000)
+          doTick DNull nst
+
+doTickNoDelay :: Rockto.Types.Direction -> GSt -> EventM () (Next GSt)
+doTickNoDelay direction st = continue $ tick direction st
 
 handleEvent :: GSt -> BrickEvent () e -> EventM () (Next GSt)
 handleEvent st (VtyEvent (V.EvKey key [])) =
-    case key of
-        V.KEsc      -> halt st
-        V.KChar 'q' -> halt st
-        V.KUp       -> continue $ tick DUp st
-        V.KDown     -> continue $ tick DDown st
-        V.KLeft     -> continue $ tick DLeft st
-        V.KRight    -> continue $ tick DRight st
-        _           -> continue st
+  case key of
+    V.KEsc      -> halt st
+    V.KChar 'q' -> halt st
+    V.KUp       -> doTickNoDelay DUp st
+    V.KDown     -> doTickNoDelay DDown st
+    V.KLeft     -> doTickNoDelay DLeft st
+    V.KRight    -> doTickNoDelay DRight st
+    _           -> continue st
 handleEvent st _ = continue st
 
 app :: App GSt e ()
 app =
-    App { appDraw = drawUI
-        , appHandleEvent = handleEvent
-        , appAttrMap = const theMap
-        , appStartEvent = return
-        , appChooseCursor = neverShowCursor
-        }
+  App
+    { appDraw = drawUI
+    , appHandleEvent = handleEvent
+    , appAttrMap = const theMap
+    , appStartEvent = return
+    , appChooseCursor = neverShowCursor
+    }
 
 main :: IO ()
 main = do
-    n <- R.newStdGen
-    void $ defaultMain app $ mkInitS n
+  n <- R.newStdGen
+  void $ defaultMain app $ mkInitS n
