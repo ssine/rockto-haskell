309c66ae851c9db4e97dd91ba2708aebbe02374d
shijiesun-edu <shs001@ucsd.edu>
2021-11-29 03:05:46 +0800

    add game instruction

--- app/UI.hs
+++ app/UI.hs
@@ -9,8 +9,8 @@ import qualified Data.List as L
 import Control.Monad (forever, void)
 import qualified Graphics.Vty as V
 
-import Brick (Widget, str, withBorderStyle, (<+>))
-import Brick.Widgets.Border (borderWithLabel, vBorder)
+import Brick (Widget, str, withBorderStyle, (<+>), (<=>))
+import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder)
 import Brick.Widgets.Border.Style (unicode)
 import Brick.Widgets.Center (center)
 
@@ -21,14 +21,20 @@ drawUI st = [ui]
          borderWithLabel (str appName)
          (center (str $ L.intercalate "\n" (map unwords (getMapRepr st)))
            <+> vBorder
-           <+> center (str $ "Round: " ++ (show . _round $ st)
+           <+> (center (str $ "Round: " ++ (show . _round $ st)
                         ++ "\nTarget: " ++ (show . _target $ st)
                         ++ "\nPos: " ++ (show . _pos $ st)
                         ++ "\nDead: " ++ (show . _dead $ st)
                         ++ "\nDropping: " ++ (show . _droppingPositions $ st)
                         ++ "\nStable: " ++ (show . _stable $ st)
-                        ++ "\nFinish: " ++ (show . _finish $ st)
-                      ))
+                        ++ "\nFinish: " ++ (show . _finish $ st))
+           <=> hBorder
+           <=> center (str $ "  === USAGE ==="
+                       ++ "\nMove:      ↑ ↓ ← →"
+                       ++ "\nRestart:   1 / r"
+                       ++ "\nQuit:      Esc / q"
+                       ++ "\nNew Game:  2 / s"
+                      )))
 
 getMapRepr :: GSt  -> [[String]]
 getMapRepr st = setList strList y $ setList (strList !! y) x "λ"
