6121592d23e90f2480d2dd7a2bba5e66e13e2a1d
dofup <bow007@ucsd.edu>
2021-11-28 14:24:34 -0800

    Add Game Over interface, Refactored UI rendering

--- app/UI.hs
+++ app/UI.hs
@@ -9,8 +9,9 @@ import qualified Data.List as L
 import Control.Monad (forever, void)
 import qualified Graphics.Vty as V
 
-import Brick (Widget, str, withBorderStyle, (<+>), (<=>))
-import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder)
+import Brick (Widget, str, withBorderStyle, (<+>), (<=>), emptyWidget, padTop, vBox, padRight, padLeft, padTop, padAll, Padding(..))
+import qualified Brick.Widgets.Border.Style as BorderS
+import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder, borderWithLabel)
 import Brick.Widgets.Border.Style (unicode)
 import Brick.Widgets.Center (center)
 
@@ -19,22 +20,48 @@ drawUI st = [ui]
   where
     ui = withBorderStyle unicode $
          borderWithLabel (str appName)
-         (center (str $ L.intercalate "\n" (map unwords (getMapRepr st)))
+         (vBox[drawGame (st)
+                      , padTop (Pad 2) $ drawGameOver (_dead st)
+                      ]
            <+> vBorder
-           <+> (center (str $ "Round: " ++ (show . _round $ st)
-                        ++ "\nTarget: " ++ (show . _target $ st)
-                        ++ "\nPos: " ++ (show . _pos $ st)
-                        ++ "\nDead: " ++ (show . _dead $ st)
-                        ++ "\nDropping: " ++ (show . _droppingPositions $ st)
-                        ++ "\nStable: " ++ (show . _stable $ st)
-                        ++ "\nFinish: " ++ (show . _finish $ st))
+           <+> (drawScore (st)
            <=> hBorder
-           <=> center (str $ "  === USAGE ==="
-                       ++ "\nMove:      ↑ ↓ ← →"
-                       ++ "\nRestart:   1 / r"
-                       ++ "\nQuit:      Esc / q"
-                       ++ "\nNew Game:  2 / s"
-                      )))
+           <=> drawUsage (st)))
+           
+drawScore :: GSt -> Widget ()
+drawScore st = 
+    withBorderStyle BorderS.unicodeBold
+          $ borderWithLabel (str "Score")
+          $ center
+          $ str $ "Round: " ++ (show . _round $ st)
+                                ++ "\nTarget: " ++ (show . _target $ st)
+                                ++ "\nPos: " ++ (show . _pos $ st)
+                                ++ "\nDead: " ++ (show . _dead $ st)
+                                ++ "\nDropping: " ++ (show . _droppingPositions $ st)
+                                ++ "\nStable: " ++ (show . _stable $ st)
+                                ++ "\nFinish: " ++ (show . _finish $ st)
+
+drawUsage :: GSt -> Widget ()
+drawUsage st =
+    withBorderStyle BorderS.unicodeBold
+    $ borderWithLabel (str "Usage")
+    $ center
+    $ str $  "\nMove:      ↑ ↓ ← →"
+          ++ "\nRestart:   1 / r"
+          ++ "\nQuit:      Esc / q"
+          ++ "\nNew Game:  2 / s"
+                      
+drawGame :: GSt  -> Widget()
+drawGame st =
+  if _dead st
+     then emptyWidget 
+     else center (str $ L.intercalate "\n" (map unwords (getMapRepr st)))
+
+drawGameOver :: Bool -> Widget()
+drawGameOver dead =
+  if dead
+     then center (str "GAME OVER")
+     else emptyWidget
 
 getMapRepr :: GSt  -> [[String]]
 getMapRepr st = setList strList y $ setList (strList !! y) x "λ"
