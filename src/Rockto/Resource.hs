f9f3968e9a7129fe96f7d925851faa615f81fdb4
ZJH <jiz056@ucsd.edu>
2021-12-04 14:58:51 +0800

    add maps and fix bugs

--- src/Rockto/Resource.hs
+++ src/Rockto/Resource.hs
@@ -1,76 +1,69 @@
 module Rockto.Resource
-  ( loadGame
+  ( loadGame,
   )
 where
 
+import Data.Char (isDigit)
+import Data.List (elemIndex, intersect)
+import Data.Maybe (fromJust)
+import Rockto.Types
 import System.IO (hFlush, stdout)
 
-import Rockto.Types
-import Data.Char (isDigit)
-import Data.List (intersect, elemIndex)
-import Data.Maybe (fromJust)
-
-
-loadMap :: FilePath -> IO (Map,(Int,Int))
+loadMap :: FilePath -> IO (Map, (Int, Int))
 loadMap filename = do
-        contents <- readFile filename
-        let alineofMap = (lines contents)!!0 
-            width_of_Map =length alineofMap
-            mapcontents = concat (lines contents)
-            list_of_Tile = parseMapString mapcontents
-            mymap =splitEvery width_of_Map list_of_Tile
-            (x,y)=findPos 's' mapcontents width_of_Map
-        return $ (Map mymap,(x,y))
+  contents <- readFile filename
+  let alineofMap = (lines contents) !! 0
+      width_of_Map = length alineofMap
+      mapcontents = concat (lines contents)
+      list_of_Tile = parseMapString mapcontents
+      mymap = splitEvery width_of_Map list_of_Tile
+      (x, y) = findPos 's' mapcontents width_of_Map
+  return $ (Map mymap, (x, y))
 
+findPos :: Char -> [Char] -> Int -> (Int, Int)
+findPos c str width = (rem, ans)
+  where
+    index = fromJust (elemIndex c str)
+    ans = index `div` width
+    rem = index `mod` width
 
-findPos :: Char-> [Char] -> Int ->(Int, Int)
-findPos c str width = (rem, ans) 
-  where index = fromJust (elemIndex c str)
-        ans   = index `div` width
-        rem   = index `mod` width
- 
 loadGame :: Int -> IO Round
 loadGame round = do
   mapInfo <- loadMap $ getFileName round
   let roundMap = fst mapInfo
       startPos = snd mapInfo
-  targetNum <- countTarget roundMap
-  return Round{ _roundMap=roundMap
-                , _roundStartPosition=startPos
-                , _roundTargetNum=targetNum}
-
-
+      targetNum = countTarget roundMap
+  return
+    Round
+      { _roundMap = roundMap,
+        _roundStartPosition = startPos,
+        _roundTargetNum = targetNum
+      }
 
 getFileName :: Int -> FilePath
-getFileName a = "resources/round_0"++show a++".txt"
+getFileName a = "resources/round_0" ++ show a ++ ".txt"
 
-countTarget :: Map -> IO Int
-countTarget gameMap = do
-  let targetNum = countParcel (concat (getMap gameMap) ) TParcel
-  return targetNum
-
+countTarget :: Map -> Int
+countTarget gameMap = countParcel (concat (getMap gameMap)) TParcel
 
 countParcel :: [Tile] -> Tile -> Int
-countParcel listTile t = length $ filter (== t ) listTile
-
+countParcel listTile t = length $ filter (== t) listTile
 
 splitEvery :: Int -> [a] -> [[a]]
 splitEvery _ [] = []
 splitEvery n list = first : (splitEvery n rest)
   where
-    (first,rest) = splitAt n list
+    (first, rest) = splitAt n list
 
 charToTile :: Char -> Tile
 charToTile ch
-      | ch =='o'= TBrick
-      | ch =='p'= TParcel
-      | ch =='x'= TWall
-      | ch =='-'= TScaffold
-      | ch =='c'= TExit
-      | ch ==' '= TEmpty
-      | ch =='s'= TEmpty
+  | ch == 'o' = TBrick
+  | ch == 'p' = TParcel
+  | ch == 'x' = TWall
+  | ch == '-' = TScaffold
+  | ch == 'c' = TExit
+  | ch == ' ' = TEmpty
+  | ch == 's' = TEmpty
 
 parseMapString :: [Char] -> [Tile]
 parseMapString str = map charToTile str
-
-
