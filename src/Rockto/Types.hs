869f6acfced4da3def7c267998e541bac1ba012a
shijiesun-edu <shs001@ucsd.edu>
2021-12-09 00:06:42 +0800

    add some comments

--- src/Rockto/Types.hs
+++ src/Rockto/Types.hs
@@ -16,7 +16,6 @@ data Tile
   | TPlayer             -- No place but UI can use it
   deriving (Eq, Ord, Enum, Show)
 
--- not sure any other necessary attributes?
 newtype Map
   = Map { getMap :: [[Tile]]
         } deriving (Show)
@@ -27,15 +26,19 @@ newtype Map
 
 data GSt
   = GSt { _map               :: Map
-        , _droppingPositions :: [(Int, Int)]
-        , _seed              :: R.StdGen
-        , _target            :: Int
-        , _round             :: Int
-        , _dead              :: Bool
-        , _stable            :: Bool
-        , _finish            :: Bool
-        , _pos               :: (Int, Int)
+        , _droppingPositions :: [(Int, Int)]    -- used by "animation"
+        , _seed              :: R.StdGen        -- global random seed
+        , _target            :: Int             -- number of remained parcels
+        , _round             :: Int             -- round of current game
+        , _dead              :: Bool            -- game status: dead
+        , _stable            :: Bool            -- used by "animation"
+        , _finish            :: Bool            -- game status: pass current round
+        , _pos               :: (Int, Int)      -- player position
         } deriving (Show)
+
+--------------------------------------------------------------------------------
+-- | Direction of Movement
+--------------------------------------------------------------------------------
 
 data Direction
   = DUp
@@ -50,8 +53,8 @@ data Direction
 --------------------------------------------------------------------------------
 
 data Game =
-  Game { _gameRound     :: Int
+  Game { _gameRound     :: Int            -- round of the game
        , _gameMap       :: Map
-       , _gameStartPos  :: (Int, Int)
-       , _gameTargetNum :: Int
+       , _gameStartPos  :: (Int, Int)     -- start position of the player
+       , _gameTargetNum :: Int            -- number of parcels to collect
        }
