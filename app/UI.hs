601dfe929ba07b105da9f448efa8d3e641e89c3a
shijiesun-edu <shs001@ucsd.edu>
2021-12-08 12:40:17 +0800

    refactor code: decouple main logic and UI-related logic

--- app/UI.hs
+++ app/UI.hs
@@ -1,46 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module UI where
+
 import Rockto.Config (appName)
-import Rockto.Types
-import Rockto.Utils
+import Rockto.Types (Direction (..), GSt (..), Tile (..), getMap)
 
-import qualified Data.List as L
-
-
-import Control.Monad (forever, void)
 import qualified Graphics.Vty as V
 
-import Brick (App (..), Widget, AttrMap, attrMap, on, fg, str, hBox, withBorderStyle, (<+>), (<=>), emptyWidget, padTop, vBox, padRight, padLeft, padTop, padAll, Padding(..), attrMap, withAttr, AttrName)
-import qualified Brick.Widgets.Border.Style as BorderS
-import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder, borderWithLabel)
+import Brick (App (..), AttrMap, AttrName, Padding (..), Widget, attrMap, emptyWidget, fg, hBox, on,
+              padAll, padLeft, padRight, padTop, str, vBox, withAttr, withBorderStyle, (<+>), (<=>))
+import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder)
 import Brick.Widgets.Border.Style (unicode)
+import qualified Brick.Widgets.Border.Style as BorderS
 import Brick.Widgets.Center (center)
-import qualified Data.Map as Map
+
 
 drawUI :: GSt -> [ Widget () ]
 drawUI st = [ui]
   where
     ui = withBorderStyle unicode $
          borderWithLabel (str appName)
-         (vBox[drawGame (st)
+         (vBox[drawGame st
                       , padTop (Pad 2) $ drawGameOver (_dead st)
-                      ]
-           <+> vBorder
-           <+> (drawScore (st)
-           <=> hBorder
-           <=> drawUsage (st)))
+         ]
+          <=> hBorder
+          <=> drawScore st)
 
 drawScore :: GSt -> Widget ()
-drawScore st = 
-    withBorderStyle BorderS.unicodeBold
-          $ borderWithLabel (str "Score")
-          $ center
-          $ str $ "Round: " ++ (show . _round $ st)
-                                ++ "\nTarget: " ++ (show . _target $ st)
-                                ++ "\nPos: " ++ (show . _pos $ st)
-                                ++ "\nDead: " ++ (show . _dead $ st)
-                                ++ "\nDropping: " ++ (show . _droppingPositions $ st)
-                                ++ "\nStable: " ++ (show . _stable $ st)
-                                ++ "\nFinish: " ++ (show . _finish $ st)
+drawScore st = str $ "Round: " ++ (show . _round $ st) ++ "  Target: " ++ (show . _target $ st)
 
 drawUsage :: GSt -> Widget ()
 drawUsage st =
@@ -52,21 +39,9 @@ drawUsage st =
           ++ "\nQuit:      Esc / q"
           ++ "\nNew Game:  2 / s"
 
-drawGameOver :: Bool -> Widget()
-drawGameOver dead =
-  if dead   
-    then center (str $   "\n  █████▀██████████████████████████████████████████████"
-                    ++   "\n  █─▄▄▄▄██▀▄─██▄─▀█▀─▄█▄─▄▄─███─▄▄─█▄─█─▄█▄─▄▄─█▄─▄▄▀█"
-                    ++   "\n  █─██▄─██─▀─███─█▄█─███─▄█▀███─██─██▄▀▄███─▄█▀██─▄─▄█"
-                    ++   "\n  ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▀▀▄▄▄▄▀▀▀▄▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀"
-     )
-     else emptyWidget       
-
-getMapSize :: Map.Map k v -> Int
-getMapSize m = Map.size m
 
 drawGame :: GSt  -> Widget()
-drawGame g = 
+drawGame g =
   if _dead g
     then emptyWidget
   else
@@ -74,30 +49,102 @@ drawGame g =
   $ center
   $ vBox r
   where
-    r = [hBox $ cellsInRow r | r <- [0..6]]
-    cellsInRow y = [if a == x && b == y then showGrid Lambda else drPos (getMap (_map g) !! y !! x) | x <- [0..15]]
+    r = [hBox $ cells r | r <- [0..6]]
+    cells y = [if a == x && b == y then showGrid Lambda else drPos (getMap (_map g) !! y !! x) | x <- [0..15]]
     drPos    = showGrid . cellAt
     (a, b) = _pos g
-    cellAt c
-      | c == TExit  = Exit
-      | c ==  TWall = Wall
-      | c ==  TBrick = Brick
-      | c ==  TParcel = Parcel
-      | c ==  TScaffold = Scaffold
+    cellAt n
+      | n == TExit  = Exit
+      | n ==  TWall = Wall
+      | n ==  TBrick = Brick
+      | n ==  TParcel = Parcel
+      | n ==  TScaffold = Scaffold
       | otherwise = Empty
 
 data Cell = Exit | Wall | Scaffold | Parcel | Brick | Empty | Lambda
-showGrid :: Cell -> Widget()
-showGrid Exit = str " △ "
-showGrid Wall  = str " ■ "
-showGrid Scaffold  = str " □ "
-showGrid Parcel  = str " ○ "
-showGrid Brick  = str " ● "
-showGrid Lambda  = str " λ "
-showGrid Empty = emp
 
+
+drawGameOver :: Bool -> Widget()
+drawGameOver dead =
+  if dead
+    then withAttr goAttr $center $ str $ "\n  █████▀██████████████████████████████████████████████"
+                    ++   "\n  █─▄▄▄▄██▀▄─██▄─▀█▀─▄█▄─▄▄─███─▄▄─█▄─█─▄█▄─▄▄─█▄─▄▄▀█"
+                    ++   "\n  █─██▄─██─▀─███─█▄█─███─▄█▀███─██─██▄▀▄███─▄█▀██─▄─▄█"
+                    ++   "\n  ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▀▀▄▄▄▄▀▀▀▄▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀"
+                    ++   "\n              Press 2 to restart                      "
+
+     else emptyWidget
+
+showGrid :: Cell -> Widget()
+showGrid Exit     = withAttr exitAttr cw
+showGrid Wall     = withAttr wallAttr wall
+showGrid Scaffold = withAttr scaffoldAttr sc
+showGrid Parcel   = withAttr parcelAttr parcel
+showGrid Brick    = withAttr brickAttr br
+showGrid Lambda   = withAttr lambdaAttr lam
+showGrid Empty    = withAttr emptyAttr emp
+
+cw :: Widget ()
+cw = str $ "\n░░░░░░░░░░"
+        ++ "\n░░██████░░"
+        ++ "\n░░█░░░░░░░"
+        ++ "\n░░██████░░"
+       ++  "\n░░░░░░░░░░"
+
+wall :: Widget ()
+wall = str $ "\n─────────█"
+      ++   "\n█▄█▄█▄█▄█▐"
+      ++   "\n███┼█████▐"
+      ++   "\n█████████▐"
+
+parcel :: Widget ()
+parcel = str $ "\n░░█████╗░░"
+          ++   "\n░██╔══██╗░"
+          ++   "\n░███████║░"
+          ++   "\n░██║░░██║░"
+          ++   "\n░╚█████╔╝░"
+
+br :: Widget ()
+br = str $ "\n░░█████╗░░"
+      ++   "\n░███████╗░"
+      ++   "\n░███████║░"
+      ++   "\n░███████║░"
+      ++   "\n░╚█████╔╝░"
+
+sc :: Widget ()
+sc = str $ "\n░░░░░░░░░░"
+        ++   "\n░░░░░░░░░░"
+        ++   "\n░░░░░░░░░░"
+        ++   "\n░░░░░░░░░░"
+        ++   "\n░░░░░░░░░░"
+
+lam :: Widget ()
+lam = str $  "\n────██────"
+        ++   "\n──▄▀█▄▄▄──"
+        ++   "\n▄▀──█▄▄───"
+        ++   "\n─▄▄▄▀──▀▄─"
+        ++   "\n─▀───────▀"
 
 emp :: Widget()
-emp = str "   "
+emp = str $  "\n          "
+        ++   "\n          "
+        ++   "\n          "
+        ++   "\n          "
+        ++   "\n          "
 
+goAttr :: AttrName
+goAttr = "over"
 
+exitAttr, wallAttr, emptyAttr, scaffoldAttr, parcelAttr, brickAttr, lambdaAttr :: AttrName
+exitAttr  = "exit"
+wallAttr = "wall"
+scaffoldAttr = "scaffold"
+parcelAttr = "parcel"
+brickAttr = "brick"
+emptyAttr = "empty"
+lambdaAttr = "lambda"
+
+theMap :: AttrMap
+theMap = attrMap V.defAttr
+  [ (goAttr, fg V.red)
+  ]
